1cfcbc70f2055dc34da292489c9c0d6df952925e
qian <ph.Milaya@gmail.com>
2022-05-16 21:04:28 +0800

    finished

--- app/src/main/java/com/milaya/sunnyweather/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/milaya/sunnyweather/ui/place/PlaceAdapter.kt
@@ -1,12 +1,10 @@
 package com.milaya.sunnyweather.ui.place
 
 import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.milaya.sunnyweather.R
 import com.milaya.sunnyweather.logic.model.Place
